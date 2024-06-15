9c8a5736ab5811a72ead16957cb96c0e087f08f7
maidaritsydenov <maidaritsydenov@gmail.com>
2024-06-15 16:36:46 +0300

    добавили плеер и полосу прокрутки

--- web_app/flaskapp.py
+++ web_app/flaskapp.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, Response, jsonify, request, session, send_file
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
@@ -8,28 +10,33 @@ import cv2
 from threading import Thread
 from yolo_detect import YoloModel
 import shutil
+from datetime import datetime
+import tempfile
 
 app = Flask(__name__)
+local_yolo = YoloModel()
+
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['ARCHIVE_FOLDER'] = 'archives'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs(app.config['ARCHIVE_FOLDER'], exist_ok=True)
+
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Run")
 
+
 def generate_frames(path_x):
-    local_yolo = YoloModel()
-    yolo_output = local_yolo.video_detection(path_x)  # generator
+    print(f'DEBUG: {path_x=}')
+    current_datetime = str(datetime.now())
+    yolo_output = local_yolo.video_detection(path_x, current_datetime)  # return generator
 
     for detection_ in yolo_output:
         ref, buffer = cv2.imencode('.jpg', detection_)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    return 'last_frame'
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
@@ -37,10 +44,12 @@ def home():
     session.clear()
     return render_template('indexproject.html')
 
+
 @app.route("/webcam", methods=['GET', 'POST'])
 def webcam():
     session.clear()
     return render_template('ui.html')
+
 
 @app.route('/FrontPage', methods=['GET', 'POST'])
 def front():
@@ -56,46 +65,82 @@ def front():
         os.makedirs(full_dirpath, exist_ok=True)
         os.makedirs(os.path.join(full_dirpath, 'original'), exist_ok=True)
         save_path = os.path.join(full_dirpath, 'original', filename)
-        file.save(save_path)
-
-        result_file_path = os.path.join(app.config['UPLOAD_FOLDER'], dir_name, 'results', filename)
-
-        session['video_path'] = save_path
+        if not os.path.exists(save_path):
+            file.save(save_path)
+            session['video_path'] = save_path
+        else:
+            session['video_path'] = None
+        result_file_path = os.path.join(app.config['UPLOAD_FOLDER'], dir_name, 'results',
+                                        filename)
         session['result_file_path'] = result_file_path
     return render_template('videoprojectnew.html', form=form)
 
-@app.route('/video')
-def video():
+
+@app.route('/frames')
+def frames():
     video_path = session.get('video_path', None)
-    result_file_path = session.get('result_file_path', None)
     if video_path:
         frame = generate_frames(video_path)
-        if frame != 'last_frame':
-            return Response(frame, mimetype='multipart/x-mixed-replace; boundary=frame')
-
-        else:
-            print('DEBUG last_frame')
-            is_generated = True
-            return render_template('videoprojectnew.html', is_generated=is_generated, filename=result_file_path)
-
+        return Response(frame, mimetype='multipart/x-mixed-replace; boundary=frame')
     return jsonify({"error": "No video path in session"}), 400
+
+
+@app.route('/timecodes/<path:result_file_path>')
+def timecodes(result_file_path):
+    timecodes_path = result_file_path.replace("results", "timecodes")
+    timecodes_path = ".".join(timecodes_path.split(".")[:-1]) + ".json"
+    if timecodes_path and os.path.exists(timecodes_path):
+        with open(timecodes_path, "r") as f:
+            timecodes = json.load(f)
+        return jsonify(timecodes), 200
+    return jsonify({"error": "No timecodes path in session"}), 400
+
+
+@app.route('/check_video_status')
+def check_video_status():
+    # FIXME: Файл создается и сохраняется во время работы модели. Т.е. файл уже существует
+    # FIXME: Тут надо проверять на завершение работы модели/ наличие файла timecodes
+
+    result_file_path = session.get('result_file_path')
+    timecode_file_path = result_file_path.replace('/results/', '/timecodes/')
+    timecode_file_path = timecode_file_path.replace('.mp4', '.json')
+
+    if os.path.exists(timecode_file_path) and os.path.exists(result_file_path):
+        return jsonify({'status': 'ready', 'result_file_path': result_file_path})
+    else:
+        return jsonify({'status': 'processing'})
+
+
+@app.route('/video/<path:result_file_path>')
+def video(result_file_path):
+    if result_file_path and os.path.exists(result_file_path):
+        print("result_file_path", result_file_path)
+        return send_file(result_file_path, mimetype='video/mp4')
+    return jsonify({"error": "No video found"}), 404
+
 
 @app.route('/save_results', methods=['POST'])
 def save_results():
-    video_path = session.get('video_path', None)
+    video_path = session.get('result_file_path', None)
     if video_path:
         filename = os.path.basename(video_path)
         dir_name, _ = os.path.splitext(filename)
         folder_path = os.path.join(app.config['UPLOAD_FOLDER'], dir_name, 'results')
-        zip_file_path = os.path.join(app.config['ARCHIVE_FOLDER'], dir_name)
-        shutil.make_archive(base_name=str(zip_file_path), format='zip', root_dir=str(folder_path))
-        return send_file(path_or_file=f'{zip_file_path}.zip', as_attachment=True)
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_folder_path = os.path.join(temp_dir, dir_name)
+            shutil.copytree(folder_path, temp_folder_path)
+            zip_file_path = os.path.join(temp_dir, f'{dir_name}.zip')
+            shutil.make_archive(base_name=temp_folder_path, format='zip', root_dir=temp_folder_path)
+            return send_file(path_or_file=zip_file_path, as_attachment=True)
 
     return jsonify({"error": "No video path in session"}), 400
+
 
 @app.route('/webapp')
 def webapp():
     return Response(generate_frames(0), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- web_app/yolo_detect.py
+++ web_app/yolo_detect.py
@@ -1,5 +1,8 @@
+import json
 import math
 import os
+import shutil
+import time
 from datetime import datetime
 
 import yaml
@@ -11,94 +14,64 @@ import cv2
 import torch
 from PIL import Image
 from werkzeug.utils import secure_filename
+import tempfile
 
 WORKDIR = Path(__file__).parent.absolute()
-
+IS_DETECTED, LABELS_IN_PERIOD, SCORES_IN_PERIOD = False, [], []
 
 class YoloModel:
     def __init__(
             self,
             # model_path: str = f"http://triton:8000/yolo/",  # из контейнера
             model_path: str = f"./best_0700_fixed_birds.pt",
-            # model_path: str = f"http://localhost:8000/yolo/",  # из контейнера
             task: str = "detect",
             path_to_config: str = 'drones.yaml',
     ):
         self.model = YOLO(model_path, task=task)
         self.path_to_config = path_to_config
-        self.config = self._load_config(self.path_to_config)
+        self.config = self._load_config(path_to_config)
         self.dataset_path: str = self.config["path"]
         self.class_names: Dict = self.config["names"]
-        self.imgsz = self.config.get("imgsz", 1280)
-        self.conf = self.config.get("conf", 0.25)
-        self.iou = self.config.get("iou", 0.45)
-        self.max_det = self.config.get("max_det", 1000)
-        self.save_txt = self.config.get("save_txt", True)
-        self.save_conf = self.config.get("save_conf", True)
-        self.save_crop = self.config.get("save_crop", False)
-        self.project = self.config.get("project", './detects')
-        self.name = self.config.get("name", 'runs')
-        self.line_width = self.config.get("line_width", 3)
-        self.show_labels = self.config.get("show_labels", True)
-        self.show_conf = self.config.get("show_conf", True)
-        self.show = self.config.get("show", False)
-        self.stream_buffer = self.config.get("stream_buffer", False)
+        self.imgsz = 1280
+        self.conf = 0.25
+        self.iou = 0.45
+        self.max_det = 1000
+        self.save_crop = False
+        self.line_width = 3
+        self.show_labels = True
+        self.show_conf = True
 
         self.classes = list(self.class_names.keys()) if self.class_names is not None else [0, 1, 2, 3, 4]
-        self.class_names_list = list(self.class_names.values()) if self.class_names is not None else ["copter", "plane", "heli", "bird", "winged"]
+        self.class_names_list = list(self.class_names.values()) if self.class_names is not None else ["copter", "plane",
+                                                                                                      "heli", "bird",
+                                                                                                      "winged"]
         self.device = 0 if torch.cuda.is_available() else 'cpu'
 
-        print("\n\nINFO: model=", self.model.model_name)
+        print("\nINFO: model=", self.model.model_name)
         print(f"INFO: model config: {self.imgsz=}, {self.conf=}, {self.iou=}")
-        print('INFO: device for inference=', self.device, '\n\n')
+        print('INFO: device', self.device, '\n')
 
     def detect(self,
                source,
-               imgsz: int = None,
-               device: Union[str, int] = 0,
+               batch: int = 16,
                stream: bool = False,
+               stream_buffer: bool = False,
                save: bool = True,
-               project: str = None,
-               name: str = None,
-               conf: float = None,
-               stream_buffer: bool = None,
-               show: bool = None,
+               project: str = 'uploads',
+               name: str = 'results',
+               show: bool = False,
+               save_txt: bool = True,
+               save_conf: bool = True,
+               agnostic_nms: bool = True,
+               exist_ok: bool = True,
                ):
 
-        imgsz = self.imgsz if imgsz is None else imgsz
-        project = self.project if project is None else project
-        name = self.name if name is None else name
-        conf = self.conf if conf is None else conf
-        show = self.show if show is None else show
-        stream_buffer = self.stream_buffer if stream_buffer is None else stream_buffer
-
-        #if device == 0 and self.device == 'cpu':
-        #    device = 'cpu'
-        print('INFO: device ->', device)
-
         results = self.model(
-            source=source,
-            imgsz=imgsz,
-            device=device,
-            stream=stream,
-            project=project,
-            name=name,
-            conf=conf,
-            data=self.path_to_config,
-            max_det=self.max_det,
-            iou=self.iou,
-            save_txt=self.save_txt,
-            save_conf=self.save_conf,
-            save_crop=self.save_crop,
-            line_width=self.line_width,
-            show_labels=self.show_labels,
-            show_conf=self.show_conf,
-            classes=self.classes,
-            stream_buffer=stream_buffer,
-            save=save,
-            show=show,
-            batch=24,
-            agnostic_nms=True,
+            source=source, stream=stream, stream_buffer=stream_buffer, save=save, project=project, name=name,
+            show=show, save_txt=save_txt, save_conf=save_conf, batch=batch, agnostic_nms=agnostic_nms,
+            exist_ok=exist_ok, imgsz=self.imgsz, device=self.device, conf=self.conf, data=self.path_to_config,
+            max_det=self.max_det, iou=self.iou, save_crop=self.save_crop, line_width=self.line_width,
+            show_labels=self.show_labels, show_conf=self.show_conf, classes=self.classes,
         )
         return results
 
@@ -109,118 +82,172 @@ class YoloModel:
                 return yaml.safe_load(f)
         raise FileNotFoundError
 
+    def process_frame(self, frame, results, timecode_artifacts=None):
+        """Метод отрисовки bboxes, labels, conf на каждом frame."""
+        global IS_DETECTED, LABELS_IN_PERIOD, SCORES_IN_PERIOD, DETECTION_START_TIME
 
-    @staticmethod
-    def get_result_path(full_file_path):
-        filename = os.path.basename(full_file_path)
-        dirname, _ = os.path.splitext(filename)
-        return dirname
+        for result in results:
+            boxes = result.boxes.xyxy.cpu().numpy()
+            confidences = result.boxes.conf.cpu().numpy()
+            class_ids = result.boxes.cls.cpu().numpy().astype(int)
 
-    def video_detection(self, path_x):
-        print(f'DEBUG: {path_x=}')
-        if path_x is None:
-            raise ValueError
+            if timecode_artifacts is not None:
+                current_time = timecode_artifacts[1] if timecode_artifacts else 0
 
-        if isinstance(path_x, str) and path_x.lower().endswith(('.png', '.jpg', '.jpeg')):
-            dirname = self.get_result_path(path_x)
+                if class_ids.size > 0:
+                    if not IS_DETECTED:
+                        DETECTION_START_TIME = current_time  # Сохранение времени первой детекции
+                    IS_DETECTED = True
+                    LABELS_IN_PERIOD.extend([self.class_names[i] for i in class_ids])
+                    SCORES_IN_PERIOD.extend([float(score) for score in confidences.tolist()])
+                else:
+                    if IS_DETECTED and (current_time - DETECTION_START_TIME) > 1:
+                        # Запись данных, только если 1 секунд прошло без детекции
+                        self.save_timecodes(timecode_artifacts, DETECTION_START_TIME, current_time, LABELS_IN_PERIOD,
+                                            SCORES_IN_PERIOD)
+                        IS_DETECTED, LABELS_IN_PERIOD, SCORES_IN_PERIOD, DETECTION_START_TIME = False, [], [], None
 
-            img = cv2.imread(path_x)
-            if img is None:
-                print(f"Error: Unable to open image file {path_x}")
-                return
+            for box, score, class_id in zip(boxes, confidences, class_ids):
+                x1, y1, x2, y2 = map(int, box)
+                class_name = self.class_names[class_id]
+                label = f"{class_name}: {score:.2f}"
+                color = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-            results = self.detect(
-                source=path_x,
-                stream=True,
-                stream_buffer=False,
-                project=f'./uploads/{dirname}/',
-                name='results'
-            )
-            for result in results:
-                frame = result.orig_img
-                boxes = result.boxes.xyxy.cpu().numpy()
-                confidences = result.boxes.conf.cpu().numpy()
-                class_ids = result.boxes.cls.cpu().numpy().astype(int)
+        return frame
 
-                for box, score, class_id in zip(boxes, confidences, class_ids):
-                    x1, y1, x2, y2 = map(int, box)
-                    label = f"{self.class_names[class_id]}: {score:.2f}"
-                    color = (0, 255, 0)
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                yield frame
+    def video_detection(self, path_x, current_datetime=None):
+        """Метод детекции в зависимости от типа файла/папки/ссылки."""
 
-        # TODO: Добавить распаковку zip
-        # TODO: добавить link и folder
-        # FIXME: почему то очень большая задержка при live streaming если stream_buffer=True
-        elif isinstance(path_x, str):
-            dirname = self.get_result_path(path_x)
+        if isinstance(path_x, str):
+            filename = os.path.basename(path_x)
+            dirname, _ = os.path.splitext(filename)
 
-            cap = cv2.VideoCapture(path_x)
-            if not cap.isOpened():
-                print(f"Error: Unable to open video file {path_x}")
-                return
+            # image
+            if path_x.lower().endswith(('.png', '.jpg', '.jpeg')):
+                results = self.detect(source=path_x, stream=True, stream_buffer=False,
+                                      project=f'./uploads/{dirname}/', name='results')
+                for frame in results:
+                    yield self.process_frame(frame.orig_img, [frame])
 
-            # results = self.detect(source=path_x, stream=True, stream_buffer=True)
-            results = self.detect(
-                source=path_x,
-                stream=True,
-                stream_buffer=True,
-                project=f'./uploads/{dirname}/',
-                name='results'
-            )
+            # video
+            elif path_x.lower().endswith(('.mp4', '.avi')):
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    temp_output_path = os.path.join(temp_dir, filename)
 
-            for result in results:
-                frame = result.orig_img
-                boxes = result.boxes.xyxy.cpu().numpy()
-                confidences = result.boxes.conf.cpu().numpy()
-                class_ids = result.boxes.cls.cpu().numpy().astype(int)
+                    cap = cv2.VideoCapture(path_x)
+                    if not cap.isOpened():
+                        print(f"Error: Unable to open video file {path_x}")
+                        return
 
-                for box, score, class_id in zip(boxes, confidences, class_ids):
-                    x1, y1, x2, y2 = map(int, box)
-                    label = f"{self.class_names[class_id]}: {score:.2f}"
-                    color = (0, 255, 0)
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                yield frame
-            cap.release()
+                    fps = cap.get(cv2.CAP_PROP_FPS)
+                    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    fourcc = cv2.VideoWriter_fourcc(*'H264')  # Кодек для MP4
 
-        elif isinstance(path_x, int) and path_x == 0:
-            current_datetime = datetime.now()
+                    out = cv2.VideoWriter(temp_output_path, fourcc, fps, (width, height))
+                    results = self.detect(source=path_x, stream=True, stream_buffer=False,
+                                          save=False, save_txt=True, save_conf=True,
+                                          project=f'./uploads/{dirname}/',
+                                          name='results')
+
+                    frame_cnt = 0
+                    for result in results:
+                        ret, frame = cap.read()
+                        if not ret:
+                            break
+
+                        frame_cnt += 1
+                        current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+                        timecode_artifacts = [frame_cnt, current_time, dirname]
+
+                        processed_frame = self.process_frame(frame, [result], timecode_artifacts)
+                        out.write(processed_frame)
+
+                        yield processed_frame
+
+                    # Освобождаем ресурсы
+                    cap.release()
+                    out.release()
+                    cv2.destroyAllWindows()
+
+                    # Перемещаем временное видео в папку results
+                    new_dir = os.path.join('./uploads', dirname, 'results')
+                    os.makedirs(new_dir, exist_ok=True)
+                    final_output_path = os.path.join(new_dir, filename)
+                    shutil.move(temp_output_path, final_output_path)
+
+            # link
+            elif 'http' in path_x:
+                pass
+
+            # folder
+            else:
+                pass
+
+        # stream
+        # 172.21.0.1 - - [14/Jun/2024 23:35:00] "GET /webcam HTTP/1.1" 200 -
+        # flask-1   | [ WARN:0@1.416] global cap_v4l.cpp:999 open VIDEOIO(V4L2:/dev/video0): can't open camera by index
+        # flask-1   | [ WARN:0@1.416] global cap.cpp:342 open VIDEOIO(V4L2): backend is generally available but can't be used to capture by index
+
+        elif path_x == 0:
             dirname = f'stream_{secure_filename(str(current_datetime))}'
-
-            cap = cv2.VideoCapture(path_x)
+            cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
             if not cap.isOpened():
                 print(f"Error: Unable to open video file {path_x}")
                 return
-
-            frame_width = int(cap.get(3))
-            frame_height = int(cap.get(4))
 
             while True:
                 success, img = cap.read()
                 if not success:
                     break
 
-                results = self.detect(
-                    source=path_x,
-                    stream=True,
-                    stream_buffer=True,
-                    project=f'./uploads/{dirname}/',
-                    name='results'
-                )
+                results = self.detect(source=path_x, stream=True, stream_buffer=True,
+                                      project=f'./uploads/{dirname}/', name='results')
 
+                frame_cnt = 0
                 for result in results:
-                    frame = result.orig_img
-                    boxes = result.boxes.xyxy.cpu().numpy()
-                    confidences = result.boxes.conf.cpu().numpy()
-                    class_ids = result.boxes.cls.cpu().numpy().astype(int)
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
 
-                    for box, score, class_id in zip(boxes, confidences, class_ids):
-                        x1, y1, x2, y2 = map(int, box)
-                        label = f"{self.class_names[class_id]}: {score:.2f}"
-                        color = (0, 255, 0)
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                    yield frame
+                    frame_cnt += 1
+                    current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+                    timecode_artifacts = [frame_cnt, current_time, dirname]
+                    yield self.process_frame(frame, [result], timecode_artifacts)
             cap.release()
+
+        else:
+            raise ValueError(f'Source {path_x} is not supported.')
+
+    @staticmethod
+    def save_timecodes(timecode_artifacts, start_time, end_time, LABELS_IN_PERIOD, SCORES_IN_PERIOD):
+        """Метод для записи временных меток, меток классов в JSON файл."""
+
+        ### timecode_artifacts: [frame_cnt, current_time, dirname]
+
+        timecodes_dir = os.path.join('uploads', timecode_artifacts[2], 'timecodes')
+        os.makedirs(timecodes_dir, exist_ok=True)
+        timecodes_file = os.path.join(timecodes_dir, f'{timecode_artifacts[2]}.json')
+
+        data = {
+            "start_time": start_time,
+            "end_time": end_time,
+            "labels": LABELS_IN_PERIOD,
+            "scores": SCORES_IN_PERIOD,
+        }
+
+        if os.path.exists(timecodes_file):
+            with open(timecodes_file, 'r+') as f:
+                try:
+                    existing_data = json.load(f)
+                except json.JSONDecodeError:
+                    print('FIXME')
+                    existing_data = []
+                existing_data.append(data)
+                f.seek(0)
+                json.dump(existing_data, f, indent=4)
+        else:
+            with open(timecodes_file, 'w') as f:
+                json.dump([data], f, indent=4)
